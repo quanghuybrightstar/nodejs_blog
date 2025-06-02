463d276ff57a503d3dbd1dbcb529ef02be70d09f
Wang Huy <daoquanghuy2907@gmail.com>
2025-06-02 23:15:38 +0700

    feature: divide structure MVC

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
+const app = express();
+const port = 3000;
+
+const route = require("./routes");
+
+// Static file
+app.use(express.static(path.join(__dirname, "public")));
+
+// Sử dụng middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Http logger
+app.use(morgan("combined"));
+
+// Template engine
+app.engine("hbs", handlebars.engine({ extname: ".hbs" }));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+// routes init
+route(app);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
---
+++ src/routes/index.js
@@ -0,0 +1,29 @@
+const newsRouter = require("./news");
+const siteRouter = require("./site");
+
+const routes = (app) => {
+  // Chia MVC
+  app.use("/news", newsRouter);
+
+  app.use("/", siteRouter);
+
+  // app.get("/news", (req, res) => {
+  //   res.render("news");
+  // });
+
+  // Local host --- Hosting
+
+  // Action ---> Disptacher ---> Function handler
+
+  app.post("/search", (req, res) => {
+    // Express nhận được req -> nhưng chưa lưu vào biến body
+    // -> cần middleware -> thành phần trung gian
+    // tại sao query lại lấy được luôn -> do express tích hợp sẵn middleware cho query parameters
+    // -> cần apply vào middle ware cho req.body -> express.urlencoded/json ()
+    console.log("req =======", req.body);
+
+    res.send("search");
+  });
+};
+
+module.exports = routes;
